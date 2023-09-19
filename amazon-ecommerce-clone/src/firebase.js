9b66539273a4355aa7d80402c77886def6b3a988
Inshira Karim <karim.inshira@gmail.com>
2023-09-20 00:46:58 +0530

    Added header

---
+++ amazon-ecommerce-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyButIg5LvHswA-124pyTbKkF5EWfDzukO0",
+    authDomain: "e-clone-d1acb.firebaseapp.com",
+    projectId: "e-clone-d1acb",
+    storageBucket: "e-clone-d1acb.appspot.com",
+    messagingSenderId: "639943286208",
+    appId: "1:639943286208:web:80fd9a146d119bf0093c25",
+    measurementId: "G-SEY5BDEMX7"
+  };
